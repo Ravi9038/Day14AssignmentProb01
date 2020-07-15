29fcd0c6f1206d717cb796014b643e82012893db
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 17:43:54 +0530

    Valid Last Name

--- PetternMatching.sh
+++ PetternMatching.sh
@@ -11,3 +11,15 @@ then
 else
 	echo "Not Found"
 fi
+
+#UseCase 2
+read -p "Enter the last Name : " lastName
+
+pattern=[A-Z]{3}$
+
+if [[ $lastName =~ $pattern ]]
+then
+	echo Found
+else
+	echo "Not Found"
+fi
