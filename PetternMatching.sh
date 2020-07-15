bf00d68f2fdad7a667991fe6322cdf61b4d9cd57
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 17:47:11 +0530

    Valid Email

--- PetternMatching.sh
+++ PetternMatching.sh
@@ -23,3 +23,17 @@ then
 else
 	echo "Not Found"
 fi
+
+
+#Valid Email Pattern
+
+read -p "Enter admin email: " email
+
+emailPattern='^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$'
+
+if [[ "$email" =~ $emailPattern ]]
+then
+	echo "Email address $email is valid."
+else
+	echo "Email address $email is invalid."
+fi
