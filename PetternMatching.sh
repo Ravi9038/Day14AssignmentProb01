15fb183f7f195309324e91178eb3efe4c93a1d50
ravi9038 <ravijadhav9038@gmail.com>
2020-07-15 17:35:11 +0530

    Valid First

--- PetternMatching.sh
+++ PetternMatching.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
-echo "Welcome to pattern"
+#UseCase 1
+read -p "Enter the First Name : " firstName
+
+pattern=^[A-Z]{3}
+
+if [[ $firstName =~ $pattern ]]
+then
+	echo Found
+else
+	echo "Not Found"
+fi
